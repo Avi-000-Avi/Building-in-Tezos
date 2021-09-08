d7ba4dfef423a0e7a3c9bad16b38fa141db5c738
Avinash Toppo <atoppo267@gmail.com>
2021-09-09 01:13:08 +0530

    Removed .git

--- help-me-plz/pages/index.js
+++ help-me-plz/pages/index.js
@@ -1,22 +1,12 @@
-import React, { useState } from "react";
 import Header from '../components/Header';
 import Page from '../components/Page';
 import Questions from '../components/Question';
 import { TezosToolkit } from '@taquito/taquito'
 import { importKey } from "@taquito/signer"
-
-const tezos = new TezosToolkit('');
-
+import { TezosToolkit } from '@taquito/taquito';
 
 
 export default function Home() {
-  const [Tezos, setTezos] = useState(
-    new TezosToolkit("https://api.tez.ie/rpc/granadanet")
-  );
-
-  const contractAddress = "KT1K3XVNzsmur7VRgY8CAHPUENaErzzEpe4e";
-
-
   return (
       <div>
       <Header/>
